58b3b270ab0079f0c9bfa8d50f8fb103f8276624
Khadija_mellak_codes <mellak71khadija@gmail.com>
2025-12-08 22:54:28 +0100

    fix routes

--- E-commerce-project-springBoot-master2/E-commerce-project-springBoot-master2/JtProject/src/main/java/com/jtspringproject/JtSpringProject/utils/PasswordGenerator.java
+++
@@ -1,4 +0,0 @@
-package com.jtspringproject.JtSpringProject.utils;
-
-public class PasswordGenerato {
-}
--- E-commerce-project-springBoot-master2/E-commerce-project-springBoot/JtProject/src/main/java/com/jtspringproject/JtSpringProject/repository/CartProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jtspringproject.JtSpringProject.repository;
-
-import com.jtspringproject.JtSpringProject.model.CartProduct;
-import com.jtspringproject.JtSpringProject.model.CartProductId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartProductRepository extends JpaRepository<CartProduct, CartProductId> {
-}
